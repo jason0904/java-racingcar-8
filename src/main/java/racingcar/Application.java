8eec6ab3698fab11c718d6cda50b1f86edd21d19
Jeshin Kim <jjason0904@cau.ac.kr>
2025-10-22 16:49:27 +0900

    feat(Main): 자동차 경주 구동을 위한 코드 추가.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.Set;
+import racingcar.controller.RacingController;
+import racingcar.model.CarRepository;
+import racingcar.view.RacingCarInput;
+import racingcar.view.RacingCarOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        CarRepository carRepository = new CarRepository();
+        RacingController racingController = new RacingController(carRepository);
+
+        racingController.run();
     }
 }
