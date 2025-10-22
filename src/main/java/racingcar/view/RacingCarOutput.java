9f4c4fb20ec1df952b17f28f86b387b5dfa35feb
Jeshin Kim <jjason0904@cau.ac.kr>
2025-10-22 13:55:02 +0900

    feat(RacingCarOutput):라운드별 결과 출력, 우승자 출력 기능 구현.

---
+++ src/main/java/racingcar/view/RacingCarOutput.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.Car;
+
+public class RacingCarOutput {
+
+    private final String FINAL_WINNER_MESSAGE = "최종 우승자: ";
+    private final String ROUND_RESULT_MESSAGE = "실행 결과";
+
+    public void printFinalWinner(List<String> winnerNames) {
+        System.out.println(FINAL_WINNER_MESSAGE + String.join(", ", winnerNames));
+    }
+
+    public void printGameResult(List<Car> result) {
+        for(Car car : result) {
+            System.out.println(car.getName() + " : " + printCarMove(car.getPosition()));
+        }
+    }
+
+    public String printCarMove(int position) {
+        return "-".repeat(position);
+    }
+
+    public void printGameResultMessage() {
+        System.out.println(ROUND_RESULT_MESSAGE);
+    }
+}
